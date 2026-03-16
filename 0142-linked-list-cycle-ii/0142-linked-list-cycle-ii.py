e5dd51f7c1fc247e85a9c9c7ac8954be6b0f2edf
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-03-16 14:42:58 -0700

    Time: 48 ms (82.44%), Space: 22.8 MB (5.18%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -8,25 +8,13 @@ class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         record = set()
 
-        # x = 0
-        # while head:
-        #     print(head.val)
-        #     head = head.next
-        #     x += 1
-        #     if x == 100:
-        #         break
-
-        # return
-
-        
-        c = 0
         while head:
             if head in record:
                 return head
 
             record.add(head)
-            c += 1
-
             head = head.next
+
+        
 
         
