45a58575b4b4ca0a9afc812d40e20631a1b27fdc
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-03-24 14:48:29 -0700

    Time: 11 ms (75.84%), Space: 19.6 MB (38.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -10,7 +10,31 @@
 
 # 
 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s) == sorted(t)
+        
+
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        s_counter = defaultdict(int)
+        for char in s:
+           s_counter[char] += 1
+
+        missing = len(s_counter)
+
+        for char in t:
+            s_counter[char] -= 1
+
+            if s_counter[char] < 0:
+                return False
+
+            if s_counter[char] == 0:
+                missing -= 1
+
+        return missing == 0
+
+         
         
