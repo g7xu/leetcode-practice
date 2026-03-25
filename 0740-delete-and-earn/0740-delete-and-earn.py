07a11311f9b3e5c458631ba7a89c3eafa51662b3
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-03-25 15:23:16 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,28 +1,25 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         count = Counter(nums)
-        unqiue_nums = sorted(list(set(nums)))
+        visited = set()
 
-        if len(unqiue_nums) == 1:
-            return sum(nums)
+        @cache
+        def helper(p, nums, visited):
+            if p >= len(nums):
+                return 0
 
+            cand = []
+            # pick
+            if nums[p] + 1 not in visited and nums[p] - 1 not in visited:
+                visited = set(visited)
+                visited.add(nums[p])
+                cand.append(count[nums[p]] * nums[p] + helper(p + 1, nums, frozenset(visited)))
+                visited.remove(nums[p])
 
-        earn1 = unqiue_nums[0] * count[unqiue_nums[0]]
-        if unqiue_nums[0] + 1 == unqiue_nums[1]:
-            earn2 = max(earn1, unqiue_nums[1] * count[unqiue_nums[1]])
-        else:
-            earn2 = earn1 + unqiue_nums[1] * count[unqiue_nums[1]]
-        
-        for i in range(2, len(unqiue_nums)):
-            if unqiue_nums[i] - 1 == unqiue_nums[i - 1]:
-                temp = max(unqiue_nums[i] * count[unqiue_nums[i]] + earn1, earn2)
-            else:
-                temp = unqiue_nums[i] * count[unqiue_nums[i]] + earn2
+            # not pick
+            cand.append(helper(p + 1, nums, frozenset(visited)))
 
+            return max(cand)
 
-            earn1 = earn2
-            earn2 = temp
-
-        return earn2
-
-
+        nums = tuple(set(nums))
+        return helper(0, nums, frozenset(visited))
