5318776b9c2f2bfe7d5e879e5d8716572a9ea57b
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-03-24 18:10:36 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.py
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -2,14 +2,49 @@
 
 # however, when colliding, check the difference if more than 1, reduce that height
 
-from heapq import heappush, heappop
+# from heapq import heappush, heappop
+# class Solution:
+#     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+#         visited = set()
+#         res = []
 
+#         heap = []
+#         for i in range(len(isWater)):
+#             level = []
+#             for j in range(len(isWater[i])):
+                
+#                 if isWater[i][j]:
+#                     level.append(0)
+#                     visited.add((i, j))
+#                     heappush(heap, (0, i, j))
+#                     continue
+
+#                 level.append(-1)
+#             res.append(level)
+
+#         while heap:
+#             h, i, j = heappop(heap)
+
+#             for id, jd in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+#                 ni = i + id
+#                 nj = j + jd
+
+#                 if ni >= 0 and ni < len(res) and nj >= 0 and nj < len(res[ni]) and (ni, nj) not in visited:
+#                     res[ni][nj] = res[i][j] + 1
+#                     heappush(heap, (res[ni][nj], ni, nj))
+#                     visited.add((ni, nj))
+
+#         return res
+
+
+# from heapq import heappush, heappop
+from collections import deque
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         visited = set()
         res = []
 
-        heap = []
+        queue = deque([])
         for i in range(len(isWater)):
             level = []
             for j in range(len(isWater[i])):
@@ -17,14 +52,14 @@ class Solution:
                 if isWater[i][j]:
                     level.append(0)
                     visited.add((i, j))
-                    heappush(heap, (0, i, j))
+                    queue.append((0, i, j))
                     continue
 
                 level.append(-1)
             res.append(level)
 
-        while heap:
-            h, i, j = heappop(heap)
+        while queue:
+            h, i, j = queue.popleft()
 
             for id, jd in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
                 ni = i + id
@@ -32,12 +67,10 @@ class Solution:
 
                 if ni >= 0 and ni < len(res) and nj >= 0 and nj < len(res[ni]) and (ni, nj) not in visited:
                     res[ni][nj] = res[i][j] + 1
-                    heappush(heap, (res[ni][nj], ni, nj))
+                    queue.append((res[ni][nj], ni, nj))
                     visited.add((ni, nj))
 
         return res
-
-
 
 
             
