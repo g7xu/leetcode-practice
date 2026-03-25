1d6cf5842c78c26e7db765ab39589e7907b62618
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-03-25 12:19:11 -0700

    Time: 0 ms (100%), Space: 19.5 MB (63.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,25 +1,42 @@
+
+
+# brute force would take O(n)
+
+
+# Binary search
+
+# 
+[4,5,6,7,0,1,2]
+
+[6,7,0,1,2,4,5]
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         a = 0
         b = len(nums) - 1
 
-        while a <= b:
+        while True:
             m = (a + b) // 2
-
             print(a, b, m)
 
             if nums[m] == target:
                 return m
-
-            if nums[a] <= nums[m]:
-                if nums[a] <= target < nums[m]:
+            elif a == b:
+                break
+            # left side sorted
+            elif nums[a] <= nums[m]:
+                if nums[a] <= target <= nums[m]:
                     b = m - 1
                 else:
                     a = m + 1
             else:
-                if nums[m] < target <= nums[b]:
+                if nums[m] <= target <= nums[b]:
                     a = m + 1
                 else:
                     b = m - 1
-        
+
+            print(a, b)
+
         return -1
+            
+
