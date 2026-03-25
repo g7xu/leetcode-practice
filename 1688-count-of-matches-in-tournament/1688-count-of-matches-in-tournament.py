589a94315f6fade4926a9702cc25108e0a1e4832
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-03-25 15:46:57 -0700

    Time: 47 ms (42.17%), Space: 19.1 MB (75.72%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -1,14 +1,20 @@
-class Solution:
-    def numberOfMatches(self, n: int) -> int:
-        if n <= 1:
-            return 0
+# class Solution:
+#     def numberOfMatches(self, n: int) -> int:
+#         if n <= 1:
+#             return 0
 
-        remain =  n // 2
+#         remain =  n // 2
 
-        if n % 2 == 0:
-            return remain + self.numberOfMatches(remain)
-        else:
-            return remain + self.numberOfMatches(remain + 1)
+#         if n % 2 == 0:
+#             return remain + self.numberOfMatches(remain)
+#         else:
+#             return remain + self.numberOfMatches(remain + 1)
 
         
+# there is actually a mathmatical trick
+# every match eliminate one team, so get to only one team, we need to get rid of n - 1 team
 
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
