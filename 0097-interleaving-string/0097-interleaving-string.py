c2ece66c7e2b0f1e310586b24849342b319ae055
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-04-02 14:06:44 -0700

    Time: 59 ms (17.66%), Space: 22.6 MB (5.16%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,21 @@
+# DFS
+# at each point, can I eitehr
+# continue my chunk or switch my chunk
+# 
+
+class Solution:
+    @cache
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if not s3:
+            return not s1 and not s2
+
+        # chose s1
+        if s1 and s1[0] == s3[0]:
+            if self.isInterleave(s1[1:], s2, s3[1:]):
+                return True
+
+        if s2 and s2[0] == s3[0]:
+            if self.isInterleave(s1, s2[1:], s3[1:]):
+                return True
+
+        return False
