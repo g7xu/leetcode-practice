fd9d524629a82d9dcc2cf26155c006fbdc91bf62
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-04-01 23:00:27 -0700

    Time: 3 ms (85.32%), Space: 22.4 MB (49.04%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -12,13 +12,19 @@ class Solution:
         not_change = set()
 
         for i in range(len(board)):
-            for j in range(len(board[i])):
-                if (i == 0 or i == len(board) - 1 or j == 0 or j == len(board[i]) - 1) and (i, j) not in not_change and board[i][j] == 'O':
+            if i == 0 or i == len(board) - 1:
+                temp = range(0, len(board[i]))
+            else:
+                temp = [0, len(board[i]) - 1]
+
+            for j in temp:
+                if (i, j) not in not_change and board[i][j] == 'O':
                     queue = deque([(i, j)])
+                    not_change.add((i, j))
 
                     while queue:
                         x, y = queue.popleft()
-                        not_change.add((x, y))
+                        
 
                         for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                             nx = x + dx
@@ -26,8 +32,10 @@ class Solution:
 
                             if nx >= 0 and nx < len(board) and ny >= 0 and ny < len(board[nx]) and (nx, ny) not in not_change and board[nx][ny] == 'O':
                                 queue.append((nx, ny))
+                                not_change.add((nx, ny))
 
         
+        print(not_change)
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if (i, j) not in not_change:
