b90bb3b6385641f6cf9c1e7aeb6d62cd33b678ea
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-04-01 22:20:05 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,33 +1,36 @@
+# x == 0 or x == len(board) - 1 or y == 0 or y == len(board[x]) - 1
+
+# check surrounding 
+
+from collections import deque
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        ROWS, COLS = len(board), len(board[0])
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        not_change = set()
 
-        def capture(r, c):
-            if (r < 0 or c < 0 or r == ROWS or
-                c == COLS or board[r][c] != "O"
-            ):
-                return
-            board[r][c] = "T"
-            capture(r + 1, c)
-            capture(r - 1, c)
-            capture(r, c + 1)
-            capture(r, c - 1)
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if (i == 0 or i == len(board) - 1 or j == 0 or j == len(board[i]) - 1) and (i, j) not in not_change and board[i][j] == 'O':
+                    queue = deque([(i, j)])
 
-        for r in range(ROWS):
-            if board[r][0] == "O":
-                capture(r, 0)
-            if board[r][COLS - 1] == "O":
-                capture(r, COLS - 1)
+                    while queue:
+                        x, y = queue.popleft()
+                        not_change.add((x, y))
 
-        for c in range(COLS):
-            if board[0][c] == "O":
-                capture(0, c)
-            if board[ROWS - 1][c] == "O":
-                capture(ROWS - 1, c)
+                        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                            nx = x + dx
+                            ny = y + dy
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "T":
-                    board[r][c] = "O"
+                            if nx >= 0 and nx < len(board) and ny >= 0 and ny < len(board[nx]) and (nx, ny) not in not_change and board[nx][ny] == 'O':
+                                queue.append((nx, ny))
+
+        
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if (i, j) not in not_change:
+                    board[i][j] = 'X'
+
+                    
