2abf4cd603815cd320bf3f0fb5c5237d0d90a9d5
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-04-08 20:52:24 -0700

    Time: 4 ms (6.08%), Space: 19.2 MB (91.19%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def scoreOfString(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pre = ord(s[0])
+class Solution:
+    def scoreOfString(self, s: str) -> int:
         res = 0
+        pre = ord(s[0])
+
         for i in range(1, len(s)):
             res += abs(pre - ord(s[i]))
             pre = ord(s[i])
-            
+
         return res
