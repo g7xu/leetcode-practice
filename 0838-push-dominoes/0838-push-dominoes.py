8f0ff6409ca775fb7622fcbfc773b81bfcfdfb47
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-04-29 09:25:16 -0700

    Time: 2464 ms (5.18%), Space: 28.8 MB (12.96%) - LeetHub

---
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -0,0 +1,58 @@
+# BFS
+
+# .L.R...LR..L..
+
+# 1 sec
+# LL.RR.LLRRLL..
+
+# 2 sec
+# LL.RR.LLRRLL..
+
+# BFS 
+# with a queue 
+# ele: [(idx, dir)]
+
+# process the queu by round/sec
+# when colid set back to .
+# 
+
+from collections import deque, defaultdict
+
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        queue = deque([])
+        for idx in range(len(dominoes)):
+            if dominoes[idx] != '.':
+                if dominoes[idx] == 'L':
+                    queue.append((idx - 1, 'L'))
+                else:
+                    queue.append((idx + 1, 'R'))
+            
+        while queue:
+            to_change = defaultdict(list)
+            for _ in range(len(queue)):
+                idx, m_dir = queue.popleft()
+
+                if idx < 0 or idx >= len(dominoes) or dominoes[idx] != '.':
+                    continue
+
+                to_change[idx].append(m_dir)
+
+            for idx, dirList in to_change.items():
+                if 'L' in dirList and 'R' in dirList:
+                    continue
+
+                dominoes = dominoes[:idx] + dirList[0] + dominoes[idx + 1:]
+
+                if dirList[0] == 'R':
+                    queue.append((idx + 1, 'R'))
+                else:
+                    queue.append((idx - 1, 'L'))
+
+        return dominoes
+
+
+
+
+
+        
