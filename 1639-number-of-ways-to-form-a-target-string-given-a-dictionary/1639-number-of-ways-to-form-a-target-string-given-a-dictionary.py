33a822268ac821a533cff52e3e51202a3e1f4e98
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-05-26 20:07:26 -0700

    Time: 1610 ms (40.12%), Space: 299.3 MB (16.05%) - LeetHub

---
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -0,0 +1,31 @@
+
+
+# the hlper (i, k)
+# 
+
+from collections import defaultdict
+
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        idx_set = [defaultdict(int) for i in range(len(words[0]))]
+
+        for word in words:
+            for i in range(len(word)):
+                idx_set[i][word[i]] += 1
+
+        @cache
+        def helper(i, k):
+            if i >= len(target):
+                return 1
+
+            if k >= len(idx_set):
+                return 0
+
+            if target[i] not in idx_set[k]:
+                return helper(i, k + 1)
+
+            return helper(i + 1, k + 1) * idx_set[k][target[i]] + helper(i, k + 1)
+
+            
+
+        return helper(0, 0) % (10**9 + 7)
