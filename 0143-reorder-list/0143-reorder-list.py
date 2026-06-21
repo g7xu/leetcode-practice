855e293c8d64509333f4bdc9a8e810c908bd9da1
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-06-21 16:27:28 -0500

    Time: 3 ms (52.56%), Space: 27.7 MB (96.78%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -14,7 +14,7 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        s, f = head, head.next
+        s, f = head, head
         while f and f.next:
             s = s.next
             f = f.next.next
