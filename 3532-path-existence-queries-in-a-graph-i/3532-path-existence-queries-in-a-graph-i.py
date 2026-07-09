d8240e769124a4c51a1feedc680387c219944140
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-07-08 23:36:46 -0500

    Time: 89 ms (65.08%), Space: 50.5 MB (30.95%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -1,26 +1,49 @@
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-        max_idx = dict()
+        # max_idx = [i for i in range(n)]
 
-        slow = fast = 0
-        while fast < len(nums):
+        # s = 0
+        # for i in range(n):
+        #     if i != len(nums) and nums[i + 1] - nums[i] <= maxDiff:
 
-            if fast == len(nums) - 1 or abs(nums[fast + 1] - nums[fast]) > maxDiff:
-                while slow <= fast:
-                    max_idx[slow] = fast
-                    slow += 1
+        root = [i for i in range(n)]
 
-                fast += 1
-                continue
-
-            fast += 1
+        start = 0
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] <= maxDiff:
+                root[i] = start
+            else:
+                start = i 
 
         res = []
-        for q in queries:
-            a, b = sorted(q)
-            if max_idx[a] >= b:
-                res.append(True)
-            else:
-                res.append(False)
+        for a, b in queries:
+            res.append(root[a] == root[b])
+        return res
 
-        return res 
+
+
+
+        # max_idx = dict()
+
+        # slow = fast = 0
+        # while fast < len(nums):
+
+        #     if fast == len(nums) - 1 or abs(nums[fast + 1] - nums[fast]) > maxDiff:
+        #         while slow <= fast:
+        #             max_idx[slow] = fast
+        #             slow += 1
+
+        #         fast += 1
+        #         continue
+
+        #     fast += 1
+
+        # res = []
+        # for q in queries:
+        #     a, b = sorted(q)
+        #     if max_idx[a] >= b:
+        #         res.append(True)
+        #     else:
+        #         res.append(False)
+
+        # return res 
