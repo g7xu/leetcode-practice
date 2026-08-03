324467aa947529c3283747e5aa4687c482eb611c
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-08-02 22:48:03 -0500

    Time: 3 ms (72.6%), Space: 21.2 MB (76.25%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        sorted_intervals = sorted(intervals)
+        print(sorted_intervals)
+
+        for i in range(1, len(sorted_intervals)):
+            if sorted_intervals[i][0] < sorted_intervals[i - 1][1]:
+                return False
+
+        return True
