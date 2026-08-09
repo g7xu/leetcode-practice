a535dd69319ec95a7db303cae6045daebd9053d8
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-08-09 15:14:05 -0500

    Time: 0 ms (100%), Space: 19 MB (98.11%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,17 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         res = [0] + digits
-        idx = len(res) - 1
-        while True:
-            res[idx] += 1
-            if res[idx] != 10:
-                break
 
-            res[idx] = 0 
-            idx -= 1
+        res[-1] += 1
+        for i in range(len(res) -1, -1, -1):
+            if res[i] == 10:
+                res[i] = 0
+                res[i-1] += 1
 
-        if res[0] == 1:
-            return res
+        if res[0] == 0:
+            return res[1:]
 
-        return res[1:]
+        return res
+
+            
         
