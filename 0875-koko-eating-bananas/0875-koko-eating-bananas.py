7149e1c3028474467731ed151ade6d6ea91fb805
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-08-22 16:59:04 -0500

    Time: 219 ms (10.04%), Space: 20.8 MB (12.28%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,69 +1,40 @@
-# 11, 4
-# 7, 3 + 2 = 5
-# 6, 2 + 2 + 2 = 6
-# 3, 1 + 2 + 3 + 3 = bad
 
-# binary search
+# best k
 
+# upper bound: min(piles)
+# lower bound: max(piles)
 
-
-# [3, 6, 7, 11]
-
-# n log n
-# left if: satified
-# right if: not satified
-# pick that number going right and check against the limit
-
-# 
-
-# binary search func (l, l_idx, r, r_idx)
-
-
+# binary search to find solution
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        piles = sorted(piles)
+        
+        # Ture or false
+        def check_hr(k, piles, h):
+            total = 0
+            for pile in piles:
+                total += pile // k
+                if pile % k != 0:
+                    total += 1
 
-        def cal_cost(idx, k):
-            res = idx
-            while idx < len(piles):
-                res += piles[idx] // k 
-                res += 0 if piles[idx] % k == 0 else 1
-                idx += 1
-            return res
+                if total > h:
+                    return False
 
-        @cache
-        def find_cost(k):
-            l = 0
-            r = len(piles) - 1
-            
-            while l < r - 1:
-                m = (l + r ) // 2
+            return True
 
-                if piles[m] == k:
-                    return cal_cost(m, k)
-                elif piles[m] >= k:
-                    r = m 
-                else:
-                    l = m
+        l = 1
+        r = max(piles)
 
-            return cal_cost(l, k)
+        print(l)
 
-        lk = 1
-        rk = piles[-1]
-        while lk < rk - 1:
-            mk = (lk + rk) // 2
-            
-            mk_cost = find_cost(mk)
+        while l < r:
+            m = (l + r) // 2
+            print(l, r, m)
 
-            if mk_cost <= h:
-                rk = mk
+
+            if check_hr(m, piles, h):
+                r = m 
             else:
-                lk = mk
+                l = m + 1
 
-        if find_cost(lk) <= h:
-            return lk
-        else:
-            return rk
-
-            
+        return r
