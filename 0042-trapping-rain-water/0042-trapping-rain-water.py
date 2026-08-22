4fb94d4e9a67ebe59bbbc9ce2eb015a20868ad8c
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-08-22 16:37:12 -0500

    Time: 23 ms (6.44%), Space: 21.3 MB (6.87%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,27 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left_to_right_max = [0]
+
+        for i in height:
+            left_to_right_max.append(max(left_to_right_max[-1], i))
+
+        left_to_right_max.pop(-1)
+
+        right_to_left_max = [0]
+
+        for i in range(len(height) - 1, 0, -1):
+            right_to_left_max.append(max(
+                right_to_left_max[-1],
+                height[i]
+            ))
+
+        right_to_left_max = right_to_left_max[::-1]
+
+        print(left_to_right_max, right_to_left_max)
+
+        res = 0
+
+        for i, j, b in zip(left_to_right_max, right_to_left_max, height):
+            res += max(min(i, j) - b, 0)
+
+        return res 
