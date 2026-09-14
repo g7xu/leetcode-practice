76618d0095be9620615c09656fb687c9d152e440
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-09-14 12:48:19 +0800

    Time: 0 ms (100%), Space: 12.3 MB (53.89%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -0,0 +1,24 @@
+r""" Thinking area
+
+
+
+
+
+"""
+
+class Solution(object):
+    def isRectangleOverlap(self, rec1, rec2):
+        """
+        :type rec1: List[int]
+        :type rec2: List[int]
+        :rtype: bool
+        """
+        if rec1[1] == rec1[3] or rec1[0] == rec1[2] or rec2[1] == rec2[3] or rec2[0] == rec2[2]:
+            return False
+
+        return not (
+            rec1[2] <= rec2[0] or
+            rec2[2] <= rec1[0] or
+            rec1[3] <= rec2[1] or
+            rec2[3] <= rec1[1]
+        )
