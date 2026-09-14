b0e33b084c84836316b3ea3407ce00491e454db6
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-09-15 00:52:53 +0800

    Time: 1 ms (19.08%), Space: 12.4 MB (53.18%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -0,0 +1,52 @@
+r""" Thinking area
+
+n log n
+
+sorted
+
+two pointer
+
+first dif, and then do linear to find the responding idx and replace
+
+
+"""
+
+class Solution(object):
+    def maximumSwap(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        numList = [int(ele) for ele in list(str(num))]
+
+        best = sorted(numList, key = lambda x: -x)
+
+        p = 0
+        tmp_best = None
+        tmp = None
+        while p < len(numList):
+            if best[p] != numList[p]:
+                tmp = numList[p]
+                tmp_best = best[p]
+                numList[p] = best[p]
+                break
+            
+            p += 1
+
+        if tmp_best is not None:
+            for i in range(len(numList) - 1, -1, -1):
+                if numList[i] == tmp_best:
+                    numList[i] = tmp
+                    break
+
+
+        res = 0
+        for i in range(len(numList)):
+            res *= 10
+            res += numList[i]
+        
+        return res
+
+
+
+            
