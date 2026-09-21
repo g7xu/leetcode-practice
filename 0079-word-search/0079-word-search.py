5ea841cebc63287bdf348923bdaaf3d04339403b
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-09-21 12:21:57 +0800

    Time: 7369 ms (10.34%), Space: 12.4 MB (58.41%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,65 +1,78 @@
+r""" Thinking area
+
+dfs with memo ctual just DFS
+
+
+"""
+
+# ideas
+# 
+
+
+# terminal
+## find the word
+## no where to go
+## wrong char
+
+# current
+# check the word
+# next one
 
 # input
-## board
-## visited
-## word
-## i
-## cor
-
-# ter
-## word[i] != board[cor]
-    ## return false
-
-## if i == len(word) - 1
-    ## return True
-
-
-# cur
-## mark curr cor visited
-## check three directions
-## check if in the bound and visited
-## check func
-## if getting true, return true 
-## mark curr cor visited
+# word
+# cur_ind
+# visit
 
 # output
-# return False
+# no
+
+# backtracking
 
 
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        n = len(board)
+        m = len(board[0])
 
-        def helper(board, visited, word, i, cor):
-            r, c = cor
-            
-            if word[i] != board[r][c]:
+
+
+        def search(visitd, word_idx, x, y):
+
+            if board[x][y] != word[word_idx]:
                 return False
-
-            if i == len(word) - 1:
+            elif word_idx == len(word) - 1:
                 return True
 
-            visited[r][c] = 1
-            for mr, mc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-                nr = r + mr
-                nc = c + mc
+            visitd.add((x, y))
+            res = False
+            for x_d, y_d in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+                nx = x + x_d
+                ny = y + y_d
 
-                if nr >= 0 and nr < len(visited) and nc >= 0 and nc < len(visited[nr]) and visited[nr][nc] == 0:
-                    re = helper(board, visited, word, i + 1, (nr, nc))
+                if nx >= 0 and nx < n and ny >= 0 and ny < m and (nx, ny) not in visitd:
+                    res = search(visitd, word_idx + 1 , nx, ny)
 
-                    if re:
-                        return True 
+                if res:
+                    break
 
-            visited[r][c] = 0
+            visitd.remove((x, y))
+            return res
 
-            return False
 
-        for r in range(len(board)):
-            for c in range(len(board[r])):
-                s_re = helper(board, [[0] * len(r) for r in board], word, 0, (r, c))
-
-                if s_re:
+        for i in range(n):
+            for j in range(m):
+                if search(set(), 0, i, j):
                     return True
 
-
         return False
+
+
+            
+
+
+        
