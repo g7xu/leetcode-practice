3c2276cf8d48e247f87f06df950f198dfbf9081e
Guoxuan Xu <guoxuan.xu8@gmail.com>
2026-09-24 13:28:00 +0800

    Time: 2 ms (40.93%), Space: 19.4 MB (18.6%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+            
+        prefix_zero = [0]
+
+        for char in s:
+            prefix_zero.append(
+                prefix_zero[-1] + (1 if char == '0' else 0)
+            )
+
+        prefix_zero = prefix_zero[1:-1]
+
+        postfix_one = [0]
+
+        for i in range(len(s) - 1, -1, -1):
+            postfix_one.append(
+                postfix_one[-1] + (1 if s[i] == '1' else 0)
+            )
+
+        postfix_one = postfix_one[::-1][1:-1]
+
+
+        print(prefix_zero)
+        print(postfix_one)
+
+        res = 0
+
+        for a, b in zip(prefix_zero, postfix_one):
+            res = max(
+                res,
+                a + b
+            )
+
+        return res
